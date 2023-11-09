250d723ff581a425504f1ecf47f657067675d5c4
yshagit31 <yshashank024@gmail.com>
2023-11-09 23:47:51 +0530

    Final

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 var typed = new Typed(".movingtext", {
   strings: [ "Frontend Web Developer."],
   typeSpeed: 50,
@@ -5,3 +6,70 @@ var typed = new Typed(".movingtext", {
   backDelay: 100,
   loop: false,
 });
+
+  document.getElementById("locationIcon").addEventListener("click", function(event) {
+    event.preventDefault(); // Prevent the default behavior of the anchor tag
+    // Open the location in a new tab or window
+    var locationURL = "https://www.google.com/maps/place/JSSATE+BOYS+HOSTEL,+Reflex+Housing+Layout,+Hemmigepura+Ward+198,+Rajarajeshwari+Nagar,+Bengaluru,+Karnataka+560098/@12.9047395,77.5065426,17z/data=!4m6!3m5!1s0x3bae3f0fbc70160d:0xdf72550b78bf1e00!8m2!3d12.9048806!4d77.5065814!16s%2Fg%2F11n6spz5zh";
+    window.open(locationURL, "_blank");
+  });
+
+var number=document.getElementsByClassName("percent1");
+var numbe=document.getElementsByClassName("percent2");
+var numb=document.getElementsByClassName("radialbars");
+var i=0;
+let a=setInterval(function()
+{
+  if(i==81){
+    clearInterval(a);
+  }
+  else
+  {
+    number[0].innerHTML=i+"%";
+    i++;
+  }
+},8);
+
+var i2=0;
+ b=setInterval(function()
+{
+  if(i2==75){
+    clearInterval(b);
+  }
+  else
+  {
+    i2++;
+    numbe[0].innerHTML=i2 +"%";
+    
+  }
+},8);
+
+var i3=0;
+ c=setInterval(function()
+{
+  if(i3>70){
+    clearInterval(c);
+  }
+  else
+  {
+    numb[2].childNodes[5].innerHTML=i3+"%";
+    i3++;
+  }
+},8);
+
+var i4=0;
+ d=setInterval(function()
+{
+  if(i4>85){
+    clearInterval();
+  }
+  else
+  {
+    numb[3].childNodes[5].innerHTML=i4+"%";
+    i4++;
+  }
+},6);
+
+
+
+
